b52016017a760312a639e2355aa1a2acd2b544ea
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2023-03-26 19:49:36 -0400

    writeCards and countDown labs complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+
+function writeCards(listOfNames,event){
+    const cardsToReturn = [];
+    for(let i=0; i<listOfNames.length; i++){
+        cardsToReturn.push(`Thank you, ${listOfNames[i]}, for the wonderful ${event} gift!`);
+    }
+    return cardsToReturn;
+}
+
+writeCards(["Guadalupe","Ollie","Aki"],"surprise");
+
+function countDown(posInteger){
+    let countNum = posInteger;
+    while(countNum > 0){        //Only will console.log values to 1 since the > will not work once countNum == 0
+        console.log(countNum);
+        countNum--;
+    }
+    console.log(countNum);      //add console.log after the loop to always console.log the value of 0
+}
+
